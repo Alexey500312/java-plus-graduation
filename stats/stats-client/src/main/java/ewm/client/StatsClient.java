8c163622beafa80fe58ab1f8f8502a6964f0eee6
Alexey <e.al.vl@yandex.ru>
2025-08-04 01:27:26 +0300

    Graduation project stage 1 spring-cloud

--- stats/stats-client/src/main/java/ewm/client/StatsClient.java
+++ stats/stats-client/src/main/java/ewm/client/StatsClient.java
@@ -2,6 +2,9 @@ package ewm.client;
 
 import ewm.CreateEndpointHitDto;
 import ewm.EndpointStatDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +16,14 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class StatsClient {
-    private final RestClient restClient = RestClient.builder().baseUrl("http://stats-server:9090").build();
+    private static final String STATS_DEFAULT_URL = "http://stats-server:9090";
+    private final DiscoveryClient discoveryClient;
+    private int oldIndexService = -1;
 
     public ResponseEntity<Void> sendHit(CreateEndpointHitDto createEndpointHitDto) {
-        return restClient.post()
+        return getRestClient().post()
                 .uri("/hit")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(createEndpointHitDto)
@@ -26,7 +32,7 @@ public class StatsClient {
     }
 
     public ResponseEntity<List<EndpointStatDto>> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
-        return restClient.get()
+        return getRestClient().get()
                 .uri(uriBuilder -> uriBuilder.path("/stats")
                         .queryParam("start", start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
                         .queryParam("end", end.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
@@ -36,5 +42,16 @@ public class StatsClient {
                 .retrieve()
                 .toEntity(new ParameterizedTypeReference<>() {
                 });
+    }
+
+    private RestClient getRestClient() {
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("stats-server");
+        if (serviceInstances != null && !serviceInstances.isEmpty()) {
+            oldIndexService = serviceInstances.size() - 1 > oldIndexService ? ++oldIndexService : 0;
+            ServiceInstance serviceInstance = serviceInstances.get(oldIndexService);
+            return RestClient.builder().baseUrl(serviceInstance.getUri().toString()).build();
+        } else {
+            return RestClient.builder().baseUrl(STATS_DEFAULT_URL).build();
+        }
     }
 }
